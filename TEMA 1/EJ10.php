23762b9837a4d576e42de2d5c04fcf6287dd7ca2
EmparanoYao Ming <EmparanoYao Ming>
2016-09-23 10:37:33 +0200

    Ejercicios TEMA 1

---
+++ TEMA 1/EJ10.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        Echo "&nbsp","&nbsp","&nbsp","&nbsp", "*","<br>";
+        Echo "&nbsp", "&nbsp","&nbsp","*","*","<br>";
+        Echo "&nbsp","&nbsp","*","*","*","<br>";
+        Echo "&nbsp","*","*","*","*","<br>";
+        
+        ?>
+    </body>
+</html>
